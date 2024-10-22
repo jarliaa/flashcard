066f3d6db55298ea465f97f550d85d330a96b0a4
jᥲrᥣιᥲᥲ <148370627+jarliaa@users.noreply.github.com>
2024-10-22 16:14:48 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
+    'O que é Python',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
+    'Qual a capital da França?', 
     'A capital da França é Paris'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'uma função é um bloco de  que executa alguma tarefa'
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês',
-    'Oi em inglês é HI (RAI)'
+    'lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
     'Biologia',
-    'Quem é conhsiderado o pai da genética?',
+    'Quem é considerado o pai da genética?',
     'Gregor Mendel'
 )
 
 criaCartao(
     'Matemática',
-    'Qual é o vallor de Pi?',
+    'Qual é o valor de Pi?',
     '3,14'
 )
